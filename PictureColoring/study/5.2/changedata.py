777e1808b5a4ebe9a71f7c2b86c8a44707314a57
chii <853879993@qq.com>
2019-07-06 14:00:04 +0800

    up

--- PictureColoring/study/5.2/changedata.py
+++ PictureColoring/study/5.2/changedata.py
@@ -2,7 +2,7 @@ import os, shutil
 
 original_dataset_dir = '.\\kaggle_original_data'
 base_dir = '.\\cats_and_dogs_small'
-# os.mkdir(base_dir)
+os.mkdir(base_dir)
 train_dir = os.path.join(base_dir, 'train')
 os.mkdir(train_dir)
 validation_dir = os.path.join(base_dir, 'validation')
@@ -21,32 +21,32 @@ test_cats_dir = os.path.join(test_dir, 'cats')
 os.mkdir(test_cats_dir)
 test_dogs_dir = os.path.join(test_dir, 'dogs')
 os.mkdir(test_dogs_dir)
-fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+fnames = ['cat.{}.jpg'.format(i) for i in range(2000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(train_cats_dir, fname)
     shutil.copyfile(src, dst)
-fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+fnames = ['cat.{}.jpg'.format(i) for i in range(2000, 2500)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(validation_cats_dir, fname)
     shutil.copyfile(src, dst)
-fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+fnames = ['cat.{}.jpg'.format(i) for i in range(2500, 3000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(test_cats_dir, fname)
     shutil.copyfile(src, dst)
-fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+fnames = ['dog.{}.jpg'.format(i) for i in range(2000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(train_dogs_dir, fname)
     shutil.copyfile(src, dst)
-fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+fnames = ['dog.{}.jpg'.format(i) for i in range(2000, 2500)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(validation_dogs_dir, fname)
     shutil.copyfile(src, dst)
-fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+fnames = ['dog.{}.jpg'.format(i) for i in range(2500, 3000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(test_dogs_dir, fname)
